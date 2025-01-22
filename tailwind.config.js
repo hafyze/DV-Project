4b5265e34373db3f06c211b0561bff3ddba571f3
hafyze <hafizmatqwer@gmail.com>
2025-01-22 23:04:10 +0800

    feat: added tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+        "./index.html",
+        "./js/**/*.js", // Include all JavaScript files in the `js` folder
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
