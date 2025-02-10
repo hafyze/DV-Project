4645f8429dd1299204c2d6e613718626e96e0165
hafyze <hafizmatqwer@gmail.com>
2025-02-10 14:30:51 +0800

    fix: chrolopleth filter default value

--- scripts/chrolopleth.js
+++ scripts/chrolopleth.js
@@ -189,7 +189,7 @@ document.addEventListener("DOMContentLoaded", () => {
         adjustLayout();
     }
 
-        updateMap("2023"); // Default year
+        updateMap("all"); // Default year will load all upon loading to page
 
     document.getElementById("yearSelect").addEventListener("change", (event) => {
         updateMap(event.target.value);
